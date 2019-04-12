4e8cad375017755d88a1e307cd6fe3bbdc429ead
Artsiom <40956624+ArtemiusSnape@users.noreply.github.com>
2019-04-12 21:32:06 +0300

    Задача о назначениях

--- Source.cpp
+++ Source.cpp
@@ -1,100 +1,110 @@
-#include <fstream>
-#include <vector>
-#include<queue>
+#include<iostream>
+#include<fstream>
+#include<vector>
+
 using namespace std;
 
-ifstream fin("input.txt");
-ofstream fout("output.txt");
-
-struct dvizh {
-	int x, y;
-
-};
-
+const int INFINIT = 10000000;
+const int INF2 = 1000000000;
 
 int main()
 {
-	
 
-	int n, m;
-	fin >> n >> m;
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
 
-	vector<vector<int>>G(n);
-	for (int i = 0; i < n; i++)
-		G[i].resize(m);
-	int i;
-	int k = 0, e = 0;
-	while (fin >> i)
-	{
-		G[k][e] = i;
-		e++;
-		if (e == m)
+
+	int n;
+	fin >> n;
+
+	vector<vector<int>>a(n + 1);
+	for (int i = 0; i < n + 1; i++)
+		for (int j = 0; j < n + 1; j++)
 		{
-			k++;
-			e = 0;
+			a[i].push_back(-1);
 		}
+
+	int ki;
+	int count = 1;
+	while (fin >> ki)
+	{
+		if (ki == 0)
+		{
+			fout << -1;
+			return 0;
+		}
+		for (int j = 0; j < ki; j++)
+		{
+			int day, risk;
+			fin >> day;
+			fin >> risk;
+			a[count][day] = risk;
+		}
+		count++;
 	}
 
-	dvizh hod[4] = {
-		{0,-1},{-1,0},{0,1},{1,0}
-	};
-
-
-	vector<vector<char>>used(n);
-	for (int j = 0; j < n; j++)
-		used[j].resize(m);
-
-	int answer = 0;
-
-	for (int j = 0; j < n; j++)
-	{
-		for (int r = 0; r < m; r++)
+	for (int i = 1; i < n + 1; i++)
+		for (int j = 1; j < n + 1; j++)
 		{
-			if (G[j][r] != -1)
-			{
-				if (!used[j][r])
+			if (a[i][j] == -1)
+				a[i][j] = INFINIT;
+		}
+
+
+	vector<int> u(n + 1);//для строк
+	vector<int>v(n + 1);//для столбцов
+	vector<int>p(n + 1);//для паросочетаний,номера строк
+	vector<int>way(n + 1);//минимумы для потенциала
+
+	for (int i = 1; i < n + 1; ++i)
+	{
+		p[0] = i;//то что смотрим в 0
+		int j0 = 0;//посетил 0 столб
+		vector<int> minv;
+		minv.assign(n + 1, INF2);
+
+		vector<char> used;
+		used.assign(n + 1, false);
+
+		do {
+			used[j0] = true;
+			int i0 = p[j0];
+			int delta = INF2;
+			int j1;
+
+			for (int j = 1; j < n + 1; ++j)
+				if (!used[j])
 				{
-					answer++;
-					queue<dvizh>q;
-					dvizh start;
-					start.x = j;
-					start.y = r;
-					q.push(start);
-					while (!q.empty())
-					{
-						dvizh U = q.front();
-						q.pop();
-						for (int i = 0; i < 4; i++)
-						{
-							dvizh pt;
-							pt.x = U.x + hod[i].x;
-							if (pt.x > n - 1 || pt.x < 0)
-								continue;
-							pt.y = U.y + hod[i].y;
-							if (pt.y > m - 1 || pt.y < 0)
-								continue;
+					int razh = a[i0][j] - u[i0] - v[j];
 
-							if (G[pt.x][pt.y] == -1)
-							{
-								used[pt.x][pt.y] = true;
-								continue;
-							}
+					if (razh < minv[j])
+						minv[j] = razh, way[j] = j0;
 
-							if (!used[pt.x][pt.y])
-							{
-								used[pt.x][pt.y] = true;
-								q.push(pt);
-
-							}
-						}
-					}
+					if (minv[j] < delta)
+						delta = minv[j], j1 = j;
 				}
-			}
-		}
 
+			for (int j = 0; j <= n; ++j)
+				if (used[j])
+					u[p[j]] += delta, v[j] -= delta;
+				else
+					minv[j] -= delta;
+
+			j0 = j1;
+
+		} while (p[j0] != 0);
+		do {
+			int j1 = way[j0];
+			p[j0] = p[j1];
+			j0 = j1;
+		} while (j0);
 	}
 
-	fout << answer;
+	if (-v[0] > INFINIT)
+		fout << -1;
+	else
+		fout << -v[0];
+
 
 	return 0;
 }
